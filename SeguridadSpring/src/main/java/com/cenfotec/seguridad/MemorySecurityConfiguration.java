bf020ffb166676f6e20ca0cf6dc14c8c306e369f
rarebeatle <amoralesm@ucenfotec.ac.cr>
2020-11-25 19:45:50 -0600

    Commit Lab9

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/MemorySecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.cenfotec.seguridad;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String encodedPassword = new BCryptPasswordEncoder().encode("ariana");
+
+		auth.inMemoryAuthentication().withUser("ariana").password(encodedPassword).roles("manager", "dev").and()
+				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+}
---
+++ SeguridadSpringBasica/src/main/java/com/cenfotec/seguridad/basica/domain/Property.java
@@ -0,0 +1,5 @@
+package com.cenfotec.seguridad.basica.domain;
+
+public class Property {
+
+}
